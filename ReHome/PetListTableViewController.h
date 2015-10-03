ba0834ce3108bf3137a9c310a5b412de9357fdf0
Jasmine <jasmine.chan89@gmail.com>
2015-10-03 13:41:03 -0400

    Add map and list view

---
+++ ReHome/PetListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PetListTableViewController.h
+//  ReHome
+//
+//  Created by JasmineChan on 2015-10-03.
+//  Copyright (c) 2015 chanizzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PetListTableViewController : UITableViewController
+
+@end
---
+++ ReHome/PetListTableViewController.m
@@ -0,0 +1,54 @@
+//
+//  PetListTableViewController.m
+//  ReHome
+//
+//  Created by JasmineChan on 2015-10-03.
+//  Copyright (c) 2015 chanizzle. All rights reserved.
+//
+
+#import "PetListTableViewController.h"
+
+@interface PetListTableViewController ()
+
+@end
+
+@implementation PetListTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 10;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PetList" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+@end
